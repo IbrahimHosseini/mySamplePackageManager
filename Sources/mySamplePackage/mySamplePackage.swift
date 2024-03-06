a61e7fbec2d4617c7040fe7084493a5e89ffd0d8
Ibrahim Hosseini <sehosseini@me.com>
2024-03-06 15:05:46 +0330

    fix: change the print function `helloWorld` to a varable that return a string value

--- Sources/mySamplePackage/mySamplePackage.swift
+++ Sources/mySamplePackage/mySamplePackage.swift
@@ -3,7 +3,7 @@
 public struct MySamplePackage {
     public init() {}
     
-    public func helloWorld() {
-        print("Hello, World!")
+    public var helloWorld: String {
+        return "Hello, World!"
     }
 }
